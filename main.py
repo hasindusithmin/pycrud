a08664e0d0642e6069c431a756f4831be3bc4e97
hasindusithmin <hasindusithmin64@gmail.com>
2022-05-24 23:46:06 +0530

    admin-add-campsite

--- main.py
+++ main.py
@@ -1,40 +1,4 @@
-from model import (Admin,CampSite,Review,AdminReview)
-from faker import Faker
-import uuid0
-from playhouse.shortcuts import model_to_dict
-fake = Faker()
-# Admin.create_table()
-# CampSite.create_table()
-# Review.create_table()
-# AdminReview.create_table()
-# admin = Admin.create(
-#     first_name = fake.first_name(),
-#     last_name = fake.last_name(),
-#     email = fake.email(),
-#     contact_number = fake.phone_number(),
-#     password = uuid0.generate().base62
-# )
+import model
 
-# camp_site = CampSite.create(
-#     name = fake.name(),
-#     address = fake.address(),
-#     location = fake.city(),
-#     contact_number = fake.phone_number(),
-#     email = fake.email(),
-#     admin = admin
-# )
-
-# review = Review.create(
-#     content = fake.text()
-# )
-
-# AdminReview.create(
-#     admin = admin,
-#     review = review
-# )
-
-# camp_site =  CampSite.get(CampSite.campsite_id == 1)
-# print(model_to_dict(camp_site))
-
-
-
+model.Admin.create_table()
+model.CampSite.create_table()
--- model.py
+++ model.py
@@ -8,36 +8,27 @@ db = peewee.PostgresqlDatabase(
     'postgres',
     user='postgres',
     password=os.getenv('PASSWORD'),
-    host='db.obkjeuaammnhmifnnyyh.supabase.co',
+    host=os.getenv('HOST'),
 )
 
 class BaseModel(peewee.Model):
     class Meta:
         database=db
 
+# Admin Add CampSite Start
 class Admin(BaseModel):
     user_id = peewee.AutoField()
-    first_name = peewee.CharField()
-    last_name = peewee.CharField()
-    email = peewee.CharField()
-    contact_number = peewee.CharField()
-    password = peewee.CharField()
-
+    first_name = peewee.CharField(max_length=25)
+    last_name = peewee.CharField(max_length=25)
+    email = peewee.CharField(max_length=55,unique=True)
+    contact_number = peewee.CharField(max_length=15)
+    password = peewee.CharField(max_length=10)
 class CampSite(BaseModel):
     campsite_id = peewee.AutoField()
-    name = peewee.CharField()
+    name = peewee.CharField(max_length=55)
     address = peewee.CharField()
-    location = peewee.CharField()
-    contact_number = peewee.CharField()
-    email = peewee.CharField()
+    location = peewee.CharField(max_length=55)
+    contact_number = peewee.CharField(max_length=15)
+    email = peewee.CharField(max_length=55,unique=True)
     admin = peewee.ForeignKeyField(Admin,related_name="admin_details")
-
-class Review(BaseModel):
-    review_id = peewee.AutoField()
-    content = peewee.CharField()
-    time = peewee.TimeField(default=datetime.datetime.now().time())
-    date = peewee.DateField(default=datetime.date.today())
-
-class AdminReview(BaseModel):
-    admin = peewee.ForeignKeyField(Admin)
-    review = peewee.ForeignKeyField(Review)
+# Admin Add CampSite End
